f5bb249840cb9f8e0a1eb61e2d81855914abf290
Tliuli <90610694+Tliuli@users.noreply.github.com>
2023-09-23 15:12:57 +0300

    3_1

---
+++ 3/3_1/3_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <typeinfo>
+#include <stdio.h>
+#include <cstdlib>
+#include <ctime>
+
+int f1(int a, int b) {
+	return a + b;
+}
+
+int f2(int a, int b) {
+	return a - b;
+}
+
+int f3(int a, int b) {
+	return a / b;
+}
+
+int f4(int a, int b) {
+	return a % b;
+}
+
+int f5(int a, int b) {
+	return a / b + a % b;
+}
+
+int (*A_links[])(int, int) = { f1, f2, f3, f4, f5 }; // массив указателей на функции
+
+int main()
+{
+	srand(unsigned(time(0)));
+	int a = rand() % 100;
+	int b = rand() % 100;
+	std::cout << a << " " << b << std::endl;
+	std::cout << f1(a, b) << std::endl;
+}
+
