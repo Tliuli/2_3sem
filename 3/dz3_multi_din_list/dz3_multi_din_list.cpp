4df4c3eefdbb5d40d72fd0e0e76e69d272f6f733
TexnarOlesa <cfytr.ru@yandex.ru>
2023-09-26 13:28:47 +0300

    Update dz3_multi_din_list.cpp

--- 3/dz3_multi_din_list/dz3_multi_din_list.cpp
+++ 3/dz3_multi_din_list/dz3_multi_din_list.cpp
@@ -3,7 +3,7 @@
 #include<time.h> //для работы функции time()
 using namespace std;
 
-int n;
+int n; // Использовать так глобальные переменные может быть опасно
 
 int coordinate(int* l, int* dim, int product_f) {
 	int address = 0;
@@ -20,6 +20,40 @@ int* copy_ar(int* Min_f) { // возможно не понадобится....
 		C_ar[i] = Min_f[i];
 	}
 	return C_ar;
+}
+
+void from_min2max(int* min, int* max, int dims) {
+	int* cur_indexes = new int[dims];
+	for (int i = 0; i < dims; i++) {
+		cur_indexes[i] = min[i];
+	}
+	int cur_index = 0;
+	while (cur_index < dims) {
+		//#Do самсинг например: чт-то(cur_indexes);
+		for (int i = 0; i < dims; i++) {
+			std::cout << cur_indexes[i] << " ";
+		}
+		std::cout << "\n";
+		//Ищем следующий подходящий индекс:
+		cur_indexes[0] += 1;
+		cur_index = 0;
+		bool is_in_bounds = cur_indexes[cur_index] < max[cur_index];
+		while (!is_in_bounds) {
+			if (cur_indexes[cur_index] < max[cur_index]) {
+				for (int j = 0; j < cur_index; j++) {
+					cur_indexes[j] = min[j];
+				}
+				is_in_bounds = true;
+			}
+			else {
+				cur_index += 1;
+				if (cur_index >= dims) {
+					break;
+				}
+				cur_indexes[cur_index] += 1;
+			}
+		}
+	}
 }
 
 int main()
@@ -63,7 +97,8 @@ int main()
 	}
 
 	// и как-то надо дальше поочередно инкрементировать значения в массиве...........
-
+	int dims = n;
+	from_min2max(Min, Max, dims);
 
 	cout << sum;
 	cout << endl;
