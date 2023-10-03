fe9763be5ca8ed19bb58cd700a999ae1164d4395
Tliuli <90610694+Tliuli@users.noreply.github.com>
2023-10-03 12:18:25 +0300

    .

--- 2/dz2_din_list/dz2_din_list.cpp
+++ 2/dz2_din_list/dz2_din_list.cpp
@@ -1,7 +1,4 @@
-﻿// 2_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 //Этот код Односвязного списка
@@ -9,7 +6,7 @@ using namespace std;
 //Добавлять элементы в конец себя и печатать себя.
 
 // structure: first-[]...<->[next, data, previous]<->...[]-last
-
+bool flag = 1; // по умолчанию - 1 - или пользовательское положение - 0 - указателя на текущий
 //Эта структура данных необходима для хранения одного элемента списка
 struct list_item {
 	int data;  //Это поле хранит данные
@@ -19,7 +16,7 @@ struct list_item {
 	//Это конструктор,
 	//Он нужен для инициализации элемента списка
 	//Пока не важно как это рабоает, используйте по аналогии с вызовом в 46 строке
-	list_item(int _data) : data(_data), next(nullptr) ,previous(nullptr) {}; // где nullptr - нулевой указатель, в Си use-ся NULL
+	list_item(int _data) : data(_data), next(nullptr), previous(nullptr) {}; // где nullptr - нулевой указатель, в Си use-ся NULL
 
 };
 
@@ -29,191 +26,284 @@ struct list {
 	list_item* first; // ан-о head 
 	list_item* last; // tail
 	list() : first(nullptr), last(nullptr) {};
+
+	bool is_list_empty() {
+		if (first == nullptr) {
+			return true;
+		}
+		return false;
+	}
+
+	list_item* pushback(int data, list_item* p) {
+		list_item* p_tmp = new list_item(data); // для указателя на временную переменную выделяем память, содержащую добавляемое значение
+		if (is_list_empty()) { // if true
+			first = p_tmp; // то указатель на текущий элемент_первый указывает на наше временное значение
+			last = p_tmp; // то указатель на текущий элемент_последний указывает на наше временное значение
+			p = p_tmp;
+			return p;
+		} // if false, то сразу переходим к след. шагу
+		last->next = p_tmp; // то указатель текущего_последнего элемента на следующий пуказывает на наше временное значение
+		p_tmp->previous = last; // "позади" нашего временный элемента находится тот, который считаем последним
+		last = p_tmp; // переносим указатель на последний элемент на наш временный. Временный теперь - последний.
+	}
+
+	// Эта штука проходится по списку и печатает элементы в порядке добавления
+	void print_list() {
+		list_item* p_tmp = first; // в качестве временного берем текущий первый
+		while (p_tmp != nullptr) { // и пока не дойдем до нулевого указателя, означающего конец
+			cout << p_tmp->data << " "; //печатаем значение текущего элемента в отдельной строке
+			p_tmp = p_tmp->next; // меняем временный на его следующий
+		}
+		std::cout << std::endl;
+	}
+	
+	void print_list_back() {
+		list_item* p_tmp = last; // в качестве временного берем текущий последний
+		while (p_tmp != nullptr) { // и пока не дойдем до нулевого указателя, означающего конец
+			cout << p_tmp->data << " "; //печатаем значение текущего элемента в отдельной строке
+			p_tmp = p_tmp->previous; // меняем временный на его предыдущий - задом наперед идем
+		}
+		std::cout << std::endl;
+	}
+	
+	list_item* pushfront(int data, list_item* p) {
+		list_item* p_tmp = new list_item(data); // для указателя на временную переменную выделяем память, содержащую добавляемое значение
+		if (is_list_empty()) { // if true
+			first = p_tmp; // то указатель на текущий элемент_первый указывает на наше временное значение
+			last = p_tmp; // то указатель на текущий элемент_последний указывает на наше временное значение
+			p = p_tmp;
+			return p;
+		} // if false, то сразу переходим к след. шагу
+		first->previous = p_tmp; // то указатель текущего_первого элемента на предыдущий пуказывает на наше временное значение
+		p_tmp->next = first; // после нашего временный элемента находится тот, который считаем первым
+		first = p_tmp; // переносим указатель на первый элемент на наш временный. Временный теперь - первый.
+		if (flag) {
+			p = p_tmp;
+			return p;
+		}
+	}
+
+	list_item* clean_list(list_item* p) {
+		list_item* p_tmp = first; // в качестве временного берем текущий первый
+		while (p_tmp->next != nullptr) { // и пока не дойдем до нулевого указателя, означающего конец
+			p_tmp = p_tmp->next; // меняем временный на его следующий
+			//cout << p_tmp->previous << endl;
+			delete p_tmp->previous; //удаляем предыдущий элемент <- Ничего не делает
+			//cout << p_tmp->previous;
+			//cout << " del ";
+			//print_list();
+		}
+		delete p_tmp;
+		p = p_tmp = nullptr;
+		flag = true;
+		//cout << "del ";
+		//print_list();
+		return p;
+	}
+
+	list_item* clean_list_back(list_item* p) {
+		list_item* p_tmp = last; // в качестве временного берем текущий последний
+		while (p_tmp->previous != nullptr) { // и пока не дойдем до нулевого указателя, означающего конец
+			p_tmp = p_tmp->previous; // меняем временный на его предыдущий - задом наперед идем
+			delete p_tmp->next; //удаляем следующий элемент <- Ничего не делает
+		}
+		delete p_tmp;
+		p = p_tmp = nullptr;
+		flag = true;
+		//cout << "del_back ";
+		//print_list();
+		return p;
+	}
+
+	list_item* pop_list_front(list_item* p) { //удаляем 1! элемент из начала
+		if (is_list_empty()) {
+			return p;
+		}
+		list_item* p_tmp = first; // в качестве временного берем текущий первый
+		first = p_tmp->next; // теперь первый элемент - тот, который следующий после времененного
+		if (p = p_tmp) {
+			if (flag) {
+				p = first;
+			}
+			else {
+				p = first;
+				flag = true;
+			}
+		}
+		if (p_tmp->next == nullptr) { //если перешли на нулевой
+			last = nullptr;
+		}
+		else {
+			p_tmp->next->previous = nullptr; //он теперь первый, ссылку на предыдущий зануляем
+		}
+		delete p_tmp; //удаляем взятый временный элемент - т.е. тот, который до махинаций был первым
+		return p;
+	}
+
+	list_item* pop_list_back(list_item* p) { //удаляем 1! элемент из начала
+		if (is_list_empty()) {
+			return p;
+		}
+		list_item* p_tmp = last; // в качестве временного берем текущий последний
+		
+		last = p_tmp->previous; // теперь последний элемент - тот, который предыдущий после времененного
+		if (p_tmp->previous == nullptr) { //если перешли на нулевой
+			first = nullptr;
+			p = nullptr;
+			flag = true;
+		}
+		else {
+			if (p == p_tmp) {
+				p = first;
+				flag = true;
+			}
+			p_tmp->previous->next = nullptr; //он теперь последний, ссылку на следующий зануляем
+		}
+		delete p_tmp; //удаляем взятый временный элемент - т.е. тот, который до махинаций был последний
+		return p;
+	}
+	// эта функция возвращает указатель на названный элемент
+	list_item* pointer(int data) { // считаем, что названный элемент точно есть в списке
+		list_item* p_tmp = first;
+		while (p_tmp->data != data) {
+			p_tmp = p_tmp->next;
+		}
+		return p_tmp;
+		flag = false;
+	}
+
+	void insert_after_cur(int data, list_item* p){
+		list_item* p_tmp = new list_item(data); // создаем указатель на новый узел, который пока еще не имеет ссылок ни на что
+		p_tmp->next = p->next; // меняем связи, ставя новый элемент после текущего из вне
+		p_tmp->previous = p;
+		p->next->previous = p_tmp;
+		p->next = p_tmp;
+	}
+
+	list_item* delete_cur(list_item* p) {
+		if (p == first) {
+			p = pop_list_front(p);
+		}
+		else if(p == last) {
+			p = pop_list_back(p);
+		}
+		else {
+			p->previous->next = p->next;
+			p->next->previous = p->previous;
+			p = first;
+			flag = true;
+		}
+		return p;
+	}
+
+	list_item* move_cur(list_item* p, bool flag_m) {
+		if (flag_m) { // flag_m пользовательская настройка: 1 - вправо, 2 - влево сдвигаем
+			p = p->next;
+		}
+		else {
+			p = p->previous;
+		}
+		return p;
+	}
+
+	list_item* divide_list_cur(list_item* p, list_item* p_1, list_item* p_2, list& l1, list& l2) {
+		list_item* p_tmp = last;
+		while (p_tmp != p) {
+			p_2 = l2.pushfront(p_tmp->data, p_2);
+			p_tmp = p_tmp->previous;
+		}
+		p_tmp = first;
+		while (p_tmp != p->next) {
+			p_1 = l1.pushback(p_tmp->data, p_1);
+			p_tmp = p_tmp->next;
+		}
+		cout << endl << "ВЫВОД ОШИБКИ" << endl; //не совсем ошибка, как поняла, особенность C\С++ из-за использования ассемблера при компиляции; т.е. вовзврат состоит только из одного элемента.
+		cout << "p_cur1 в функции перед выходом " << p_1 << endl;
+		cout << "p_cur2 в функции перед выходом " << p_2 << endl;
+		return p_1, p_2;
+	}
+
+	~list(){
+		cout << "Деструктор list" << endl;
+	}
 };
-
-
-// Тут все ясно
-bool is_list_empty(list* my_list) {
-	if (my_list->first == nullptr) {
-		return true;
-	}
-	return false;
-}
-
-// Здесь реализовано добавление нового элемента в конец списка
-// Как только вы поймете как это работает - вы успешны
-// Оператор -> означает разыминование указателя, и обращение к полю
-// То-есть cur_list->last - Это то-же самое что (*cur_list).last
-void pushback(list* cur_list, int data) {
-	list_item* p_tmp = new list_item(data); // для указателя на временную переменную выделяем память, содержащую добавляемое значение
-	if (is_list_empty(cur_list)) { // if true
-		cur_list->first = p_tmp; // то указатель на текущий элемент_первый указывает на наше временное значение
-		cur_list->last = p_tmp; // то указатель на текущий элемент_последний указывает на наше временное значение
-		return;
-	} // if false, то сразу переходим к след. шагу
-	cur_list->last->next = p_tmp; // то указатель текущего_последнего элемента на следующий пуказывает на наше временное значение
-	p_tmp->previous = cur_list->last; // "позади" нашего временный элемента находится тот, который считаем последним
-	cur_list->last = p_tmp; // переносим указатель на последний элемент на наш временный. Временный теперь - последний.
-}
-
-void pushfront(list* cur_list, int data) {
-	list_item* p_tmp = new list_item(data); // для указателя на временную переменную выделяем память, содержащую добавляемое значение
-	if (is_list_empty(cur_list)) { // if true
-		cur_list->first = p_tmp; // то указатель на текущий элемент_первый указывает на наше временное значение
-		cur_list->last = p_tmp; // то указатель на текущий элемент_последний указывает на наше временное значение
-		return;
-	} // if false, то сразу переходим к след. шагу
-	cur_list->first->previous = p_tmp; // то указатель текущего_первого элемента на предыдущий пуказывает на наше временное значение
-	p_tmp->next = cur_list->first; // после нашего временный элемента находится тот, который считаем первым
-	cur_list->first = p_tmp; // переносим указатель на первый элемент на наш временный. Временный теперь - первый.
-}
-
-// Эта штука проходится по списку и печатает элементы в порядке добавления
-void print_list(list* cur_list) {
-	list_item* p_tmp = cur_list->first; // в качестве временного берем текущий первый
-	while (p_tmp != nullptr) { // и пока не дойдем до нулевого указателя, означающего конец
-		std::cout << p_tmp->data << std::endl; //печатаем значение текущего элемента в отдельной строке
-		p_tmp = p_tmp->next; // меняем временный на его следующий
-	}
-}
-
-void print_list_back(list* cur_list) {
-	list_item* p_tmp = cur_list->last; // в качестве временного берем текущий последний
-	while (p_tmp != nullptr) { // и пока не дойдем до нулевого указателя, означающего конец
-		std::cout << p_tmp->data << std::endl; //печатаем значение текущего элемента в отдельной строке
-		p_tmp = p_tmp->previous; // меняем временный на его предыдущий - задом наперед идем
-	}
-}
-
-void clean_list(list* cur_list) {
-	list_item* p_tmp = cur_list->first; // в качестве временного берем текущий первый
-	while (p_tmp->next != nullptr) { // и пока не дойдем до нулевого указателя, означающего конец
-		p_tmp = p_tmp->next; // меняем временный на его следующий
-		delete p_tmp->previous; //удаляем предыдущий элемент
-	}
-	delete p_tmp;
-	p_tmp = nullptr;
-}
-
-void clean_list_back(list* cur_list) {
-	list_item* p_tmp = cur_list->last; // в качестве временного берем текущий последний
-	while (p_tmp->previous != nullptr) { // и пока не дойдем до нулевого указателя, означающего конец
-		p_tmp = p_tmp->previous; // меняем временный на его предыдущий - задом наперед идем
-		delete p_tmp->next; //удаляем следующий элемент
-	}
-	delete p_tmp;
-	p_tmp = nullptr;
-}
-
-void pop_list_front(list* cur_list) { //удаляем 1! элемент из начала
-	if (is_list_empty(cur_list)) {
-		return;
-	}
-	list_item* p_tmp = cur_list->first; // в качестве временного берем текущий первый
-	cur_list->first = p_tmp->next; // теперь первый элемент - тот, который следующий после времененного
-	if (p_tmp->next == nullptr) { //если перешли на нулевой 
-		cur_list->last == nullptr;
-	}
-	else {
-		p_tmp->next->previous = nullptr; //он теперь первый, ссылку на предыдущий зануляем
-	}
-	delete p_tmp; //удаляем взятый временный элемент - т.е. тот, который до махинаций был первым
-}
-
-void pop_list_back(list* cur_list) { //удаляем 1! элемент из начала
-	if (is_list_empty(cur_list)) {
-		return;
-	}
-	list_item* p_tmp = cur_list->last; // в качестве временного берем текущий последний 
-	cur_list->last = p_tmp->previous; // теперь последний элемент - тот, который предыдущий после времененного
-	if (p_tmp->previous == nullptr) { //если перешли на нулевой 
-		cur_list->first == nullptr;
-	}
-	else {
-		p_tmp->previous->next = nullptr; //он теперь последний, ссылку на следующий зануляем
-	}
-	delete p_tmp; //удаляем взятый временный элемент - т.е. тот, который до махинаций был последний
-}
-// эта функция возвращает указатель на названный элемент
-list_item *pointer(list* cur_list, int data) { // считаем, что названный элемент точно есть в списке
-	list_item* p_tmp = cur_list->first; 
-	while (p_tmp->data != data) {
-		p_tmp = p_tmp->next;
-	}
-	return p_tmp;
-}
-
-void insert_after_cur(int data, list_item* p){
-	list_item* p_tmp = new list_item(data); // создаем указатель на новый узел, который пока еще не имеет ссылок ни на что
-	p_tmp->next = p->next; // меняем связи, ставя новый элемент после текущего из вне
-	p->next = p_tmp;
-}
-
-void delete_cur(list* cur_list, list_item* p) {
-	if (p == cur_list->first) {
-		pop_list_front(cur_list);
-	}
-	else if(p == cur_list->last) {
-		pop_list_back(cur_list);
-	}
-	else {
-		p->previous->next = p->next;
-		p->next->previous = p->previous;
-		delete p;
-	}
-}
 
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	
 	list l;
 	list_item *p_cur = l.first; // это ссылка на первый элемента списка, но она не меняется, когда что-то производим с этим элементом списка Why. -
 	//т.к. она указывает на ту ячейку памяти, куда и указатель l.first, а когда меняется первый элемент, указатель l.first мы переносим, а вот этот - нет.
-
-	//pushback(&l, 28); unneed for check
-	//pushback(&l, 37);
-	//pushback(&l, 21);
-	//pushback(&l, 4);
-	pushfront(&l, 2); // добавление в начало - success
-	pushfront(&l, 3);
-	pushfront(&l, 4);
-	pushfront(&l, 5);
-
-	//cout << l.first << "\n"; // if add ->data , то будет выводить первое значение, а не адрес
-	//cout << l.first->data << "\n"; // if add ->data , то будет выводить первое значение, а не адрес
-	//cout << p_cur << endl;
-
-	print_list(&l);
-	cout << "Check" << endl;
-	p_cur = pointer(&l, 2);
-	delete_cur(&l, p_cur);
-	//insert_after_cur(8, p_cur);
-	print_list(&l);
-	//cout << "Check" << endl;
-	//cout << pointer(&l, 4)->data << endl;
-	cout << "\n";
-	//print_list_back(&l); // печать в обратном порядке - success
-	//pop_list_front(&l);
-	pop_list_back(&l);
-	print_list(&l);
-
-	//cout << is_list_empty(&l) << std::endl; ан-о
-	//clean_list(&l);
-	clean_list_back(&l);
-	//print_list(&l);
-	//cout << "\n";
-	//print_list_back(&l);
+	cout << "Рассмотрим, какие методы есть у созданной нами структуры и как они работают." << endl;
+	cout << "Умеем добавлять элементы в конец списка. Добавим последовательно 28, 37, 41." << endl;
+	p_cur = l.pushback(28, p_cur);
+	p_cur = l.pushback(37, p_cur);
+	p_cur = l.pushback(41, p_cur);
+	//p_cur = l.pushback(&l, 28, p_cur);
+	cout << "Умеем печатать список в прямом порядке ";
+	l.print_list();
+	cout << " ...и обратном - ";
+	l.print_list_back();
+	cout << "Добавим в начало списка 2, 3, 5. Тогда: ";
+	p_cur = l.pushfront(2, p_cur);
+	p_cur = l.pushfront(3, p_cur);
+	p_cur = l.pushfront(5, p_cur);
+	l.print_list();
+	cout << "Имеем в наличии указатель на выбранный элемент - текущий - который по умолчанию указывает на первый элемент в списке." << endl;
+	cout << "Вернем значение, на которое ссылается указатель - " << p_cur->data << endl;
+	cout << "Умеем удалять элементы из конца и начала списка - " << endl;
+	cout << p_cur->data << endl;
+	p_cur = l.pop_list_back(p_cur);
+	//cout << p_cur->data << endl;
+	p_cur = l.pop_list_front(p_cur);
+	l.print_list();
+	cout << "Есть возможность назначать текущий элемент по его значению, например, ";
+	p_cur = l.pointer( 28);
+	cout << p_cur->data << endl;
+	cout << "Можем вставлять новые элементы после текущего и удалять текущий - ";
+	l.insert_after_cur(101, p_cur);
+	p_cur = l.delete_cur(p_cur);
+	l.print_list();
+	cout << "Т.к. \"текущий элемент\" был удален, указатель вернулся в начало - ";
+	cout << p_cur->data << endl;
+	cout << "Добавим новых элементов в начало. И выберем указателем элемент из ~середины - ";
+	for (int i = 8; i < 15; i++) {
+		p_cur = l.pushfront(i, p_cur);
+	}
+	p_cur = l.pointer( 9);
+	cout << p_cur->data << endl;
+	l.print_list();
+	cout << "Помимо назначения по элементу, указатель мы можем просто сдвигать вправо - " << endl;
+	p_cur = l.move_cur(p_cur, 1);
+	cout << p_cur->data << endl;
+	cout << "...и влево - " << endl;
+	p_cur = l.move_cur(p_cur, 0);
+	cout << p_cur->data << endl;
+	cout << "Также есть метод для разделения списка на две части по текущему элементу. Однако работа продолжается только с частью, включащей элемент - левой." << endl;
+	list l1;
+	list l2;
+	list_item* p_cur1 = l1.first;
+	list_item* p_cur2 = l2.first;
+	p_cur1, p_cur2 = l.divide_list_cur(p_cur, p_cur1, p_cur2, l1, l2); // в принципе, раз так, то можем получить значение как p_curi = li.first
+	cout << "p_cur1 после выхода из функции " << p_cur1 << endl;
+	cout << "p_cur2 после выхода из функции " << p_cur2 << endl;
+	cout << "-//-" << endl;
+	cout << endl << "l1" << endl;
+	l1.print_list();
+	cout << "l2" << endl;
+	l2.print_list();
+	p_cur1 = l1.first;
+	p_cur2 = l2.first;
+	cout << "И можем удалять список - не можем" << endl;
+	//p_cur = l.clean_list(p_cur);
+	//l.print_list();
+	//p_cur1 = l1.clean_list(p_cur1);
+	//p_cur2 = l2.clean_list_back(p_cur2);
+	//l1.print_list();
+	//l2.print_list();
+	
 	return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
